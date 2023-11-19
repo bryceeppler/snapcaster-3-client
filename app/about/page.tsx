02a4e05d4243b6f18625365996ca309f928d348b
Bryce Eppler <eppler97@gmail.com>
2023-11-19 10:49:11 -0800

    adding about page

---
+++ app/about/page.tsx
@@ -0,0 +1,42 @@
+import Button from "@/components/ui/Button";
+
+export default function Component() {
+  return (
+    <section className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="container mx-auto space-y-6">
+        <h2 className="text-4xl font-bold text-center text-zinc-900 dark:text-zinc-50">
+          About Snapcaster
+        </h2>
+        <div className="max-w-3xl mx-auto text-center text-zinc-600 dark:text-zinc-200">
+          <p className="text-lg">
+            This is a project that I created to help Magic: The Gathering
+            players search for singles on Canadian websites. With snapcaster,
+            you can search multiple websites at once and see all the results in
+            one place. If you have any suggestions or feedback, please feel free
+            to email me at epplerdev@gmail.com.
+          </p>
+        </div>
+        <h2 className="text-4xl font-bold text-center text-zinc-900 dark:text-zinc-50">
+          Support
+        </h2>
+        <div className="max-w-3xl mx-auto text-center text-zinc-600 dark:text-zinc-200">
+          <p className="text-lg">
+            Snapcaster is a service I created in my spare time, and I rely on
+            donations to cover the server costs. If you find snapcaster helpful,
+            please consider donating to support the project.
+          </p>
+        </div>
+        <div className="text-center">
+          <a
+            href="https://www.paypal.com/donate/?business=KK537LVP4TZ5Q&no_recurring=0&item_name=Thank+you+for+supporting+snapcaster%21+Your+generosity+keeps+snapcaster+running+by+covering+server+costs.&currency_code=CAD"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-zinc-900 text-white p-4 shadow-lg rounded-lg text-lg font-bold hover:bg-zinc-800 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-100"
+          >
+            Donate with PayPal
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ components/ui/Navbar.tsx
@@ -0,0 +1,178 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
+
+type Props = {};
+
+export default function Navbar({}: Props) {
+  const currentPath = usePathname();
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const logoSrc = "/logo.png";
+  const pages = [
+    { name: "Home", href: "/", current: currentPath === "/" },
+    {
+      name: "Multi-search",
+      href: "/multisearch",
+      current: currentPath === "/multisearch",
+    },
+    { name: "About", href: "/about", current: currentPath === "/about" },
+  ];
+  return (
+    <div>
+      <nav className="shadow">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="relative flex h-16 items-center justify-between">
+            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              {/* Mobile menu button*/}
+              <button
+                type="button"
+                className="inline-flex items-center justify-center rounded-md p-2 text-zinc-400 hover:bg-zinc-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+                onClick={() => {
+                  setMobileMenuOpen(!mobileMenuOpen);
+                }}
+              >
+                <span className="sr-only">Open main menu</span>
+                {/* Icon when menu is closed. */}
+                {/* Menu open: "hidden", Menu closed: "block" */}
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+                {/* Icon when menu is open. */}
+                {/* Menu open: "block", Menu closed: "hidden" */}
+                <svg
+                  className="hidden h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex flex-shrink-0 items-center">
+                <Image
+                  className="block lg:hidden"
+                  src="/favicon.ico"
+                  alt="snapcaster"
+                  width={32}
+                    height={32}
+                />
+                {}
+                <Image
+                  className="hidden mb-2 lg:block"
+                  src={logoSrc}
+                  alt="snapcaster"
+                    width={80}
+                    height={32}
+                />
+              </div>
+              <div className="hidden sm:ml-6 sm:block sm:w-full">
+                <div className="flex space-x-4 w-full">
+                  {pages.map((page) => (
+                    <a
+                      key={page.name}
+                      href={page.href}
+                      className={`
+ rounded-md py-2 px-3 text-sm font-medium hover:bg-zinc-600 
+${page.current && "bg-zinc-800 text-white hover:bg-zinc-600"} 
+`}
+                    >
+                      {page.name}
+                    </a>
+                  ))}
+
+                  {/* User ? */}
+                  <div className="flex-1" />
+                  {/* <div
+                    className={`
+ rounded-md py-2 px-3 text-sm font-medium hover:bg-zinc-600
+`}
+                  >
+                    {user ? (
+                      <span
+                        onClick={async () => {
+                          await supabaseClient.auth.signOut();
+                          router.push('/');
+                        }}
+                      >
+                        Sign out
+                      </span>
+                    ) : (
+                      <Link href="/signin">Sign in</Link>
+                    )}
+                  </div> */}
+                </div>
+              </div>
+            </div>
+          </div>
+          {/*  mobile menu opens when mobileMenuOpen = true */}
+          <div
+            className={`${mobileMenuOpen ? "block h-fit" : "hidden"} sm:hidden`}
+            id="mobile-menu"
+          >
+            <div className="space-y-1 px-2 pt-2 pb-3">
+              {pages.map((page) => (
+                <Link
+                  href={page.href}
+                  as={page.href}
+                  key={page.name}
+                  className={`
+                    block rounded-md py-2 px-3 text-sm font-medium
+                    ${page.current && "bg-zinc-600 text-white"} 
+                    `}
+                >
+                  {page.name}
+                </Link>
+              ))}
+              {/* USER STUFF */}
+              {/* <div
+                    className={`
+ rounded-md py-2 px-3 text-sm font-medium hover:bg-zinc-400
+`}
+                  >
+                    {user ? (
+                      <span
+                        onClick={async () => {
+                          await supabaseClient.auth.signOut();
+                          router.push('/signin');
+                        }}
+                      >
+                        Sign out
+                      </span>
+                    ) : (
+                      <Link href="/signin">Sign in</Link>
+                    )}
+                  </div> */}
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
